87f515f20a060c8066e5e28a1a15724e93e2620a
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-06-28 16:36:54 -0700

    final ran

--- sra_wang.sh
+++ sra_wang.sh
@@ -5,7 +5,7 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=12:00:00
+#SBATCH --time=15:00:00
 #SBATCH --mail-user=chandlersutherland@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
@@ -15,7 +15,7 @@ module load python
 module load sra-tools
 SCRATCH_DIR=/global/scratch/users/chandlersutherland/e12/wang_athaliana
 
-for ((i = 188; i <= 248; i++))
+for ((i = 221; i <= 248; i++))
 	do 
 	fasterq-dump --threads $SLURM_NTASKS -O $SCRATCH_DIR -t $SCRATCH_DIR -p SRR8367$i
 done 
