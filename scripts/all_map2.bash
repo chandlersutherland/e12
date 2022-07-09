3b37f4d48cb4d54aa94df010cea618442ab47f61
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-07-08 17:35:28 -0700

    editing for different file extensions

--- scripts/all_map2.bash
+++ scripts/all_map2.bash
@@ -12,24 +12,39 @@
 #SBATCH --output=/global/home/users/chandlersutherland/slurm_stdout/slurm-%j.out
 
 module load python
-#module load sra-tools
 module load bwa
 
+#name directories 
 OUTPUT_DIR=/global/scratch/users/chandlersutherland/e12/wang_athaliana/mapped_reads_rg
 INPUT_DIR=/global/scratch/users/chandlersutherland/e12/wang_athaliana/fastq_rg
 REF_GENOME=/global/scratch/users/chandlersutherland/phytozome/Athaliana/Araport11/assembly/Athaliana_447_TAIR10.fa
 
+#already indexed, so shouldn't have to again 
 #bwa index $REF_GENOME 
 
 cd $INPUT_DIR
+#initialize files 
 echo "" > basename.txt 
+echo "" > rg_basename.txt
+
+#because the raw files and the sra files have different codes, doing separately 
 for f in *.fastq
 	do basename=$(basename $f .fastq | sed 's/..$//')
 	echo $basename>>basename.txt 
 done 	
+
+for f in *.fq
+	do rg_basename=$(basename $f .fastq | sed 's/..$//')
+	echo $rg_basename>>rg_basename.txt
 
 file_name=$(cat basename.txt | sort -u) 
 
 for i in $file_name
 	do bwa mem -t $SLURM_NTASKS $REF_GENOME $INPUT_DIR/"${i}"_1.fastq $INPUT_DIR/"${i}"_2.fastq > $OUTPUT_DIR/"${i}".sam
 done	
+
+rg_file_name=$(cat rg_basename.txt | sort -u) 
+
+for i in $rg_file_name
+	do bwa mem -t $SLURM_NTASKS $REF_GENOME $INPUT_DIR/"${i}"_1.fq $INPUT_DIR/"${i}"_2.fq > $OUTPUT_DIR/"${i}".sam
+done
