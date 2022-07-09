fa45d98ebd818fcba2ebaf8289f60dd76caa7075
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-07-09 09:49:40 -0700

    didn't budget enough time, splitting up the remaining runs

--- scripts/mapping_qc_rg.bash
+++ scripts/mapping_qc_rg.bash
@@ -5,7 +5,7 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=02:00:00
+#SBATCH --time=06:00:00
 #SBATCH --mail-user=chandlersutherland@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
@@ -22,8 +22,9 @@ OUTPUT_DIR=/global/scratch/users/chandlersutherland/e12/wang_athaliana/bam_sorte
 
 #echo "sra,mean_read_depth,breadth,prop" > $OUTPUT_DIR/mapping_stats.csv
 
+diff=$(grep -Fxvf $OUTPUT_DIR/already_mapped.txt $INPUT_DIR/all_files.txt)
 cd $INPUT_DIR 
-for f in *.sam
+for f in $diff
 do 
 	basename=$(basename $f .sam)
 	OUTPUT_BAM=$OUTPUT_DIR/"${basename}".bam
@@ -42,21 +43,3 @@ do
 	echo "${name} converted and mapping statistics written to mapping_stats.csv"
 done
 
-#do it for neg control 
-INPUT_DIR=/global/scratch/users/chandlersutherland/e12/wang_athaliana/neg_control
-OUTPUT_DIR=/global/scratch/users/chandlersutherland/e12/wang_athaliana/neg_control 
-
-OUTPUT_BAM=$OUTPUT_DIR/all.bam
-echo "beginning neg_control"
-#convert to bam, sort, and index 
-samtools view -@ $SLURM_NTASKS -b $INPUT_DIR/all_aln.sam |\
-samtools sort -@ $SLURM_NTASKS -T $OUTPUT_DIR/temp > $OUTPUT_BAM
-samtools index -@ $SLURM_NTASKS $OUTPUT_BAM
-	
-#generate mapping statistics 
-name="all"
-MEAN_READ_DEPTH=$(samtools depth -a $OUTPUT_BAM | awk '{c++;s+=$3}END{print s/c}')
-BREADTH=$(samtools depth -a $OUTPUT_BAM | awk '{c++; if($3>0) total+=1}END{print (total/c)*100}')
-PROP=$(samtools flagstat $OUTPUT_BAM | awk -F "[(|%]" 'NR == 3 {print $2}')
-echo "${name},${MEAN_READ_DEPTH},${BREADTH},${PROP}" >> $OUTPUT_DIR/mapping_stats.csv
-echo "${name} converted and mapping statistics written to mapping_stats.csv"
---
+++ scripts/neg_map_qc.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=mapping_qc
+#SBATCH --account=ac_kvkallow
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=06:00:00
+#SBATCH --mail-user=chandlersutherland@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
+#SBATCH --output=/global/home/users/chandlersutherland/slurm_stdout/slurm-%j.out
+
+#do it for neg control 
+INPUT_DIR=/global/scratch/users/chandlersutherland/e12/wang_athaliana/neg_control
+OUTPUT_DIR=/global/scratch/users/chandlersutherland/e12/wang_athaliana/neg_control 
+
+OUTPUT_BAM=$OUTPUT_DIR/all.bam
+echo "beginning neg_control"
+#convert to bam, sort, and index 
+samtools view -@ $SLURM_NTASKS -b $INPUT_DIR/all_aln.sam |\
+samtools sort -@ $SLURM_NTASKS -T $OUTPUT_DIR/temp > $OUTPUT_BAM
+samtools index -@ $SLURM_NTASKS $OUTPUT_BAM
+	
+#generate mapping statistics 
+name="all"
+MEAN_READ_DEPTH=$(samtools depth -a $OUTPUT_BAM | awk '{c++;s+=$3}END{print s/c}')
+BREADTH=$(samtools depth -a $OUTPUT_BAM | awk '{c++; if($3>0) total+=1}END{print (total/c)*100}')
+PROP=$(samtools flagstat $OUTPUT_BAM | awk -F "[(|%]" 'NR == 3 {print $2}')
+echo "${name},${MEAN_READ_DEPTH},${BREADTH},${PROP}" >> $OUTPUT_DIR/mapping_stats.csv
+echo "${name} converted and mapping statistics written to mapping_stats.csv"
