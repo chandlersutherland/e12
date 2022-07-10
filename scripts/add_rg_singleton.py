a5d468d48c27133d18abd44b9d0d0dfde6c0ac9f
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-07-09 20:06:25 -0700

    change output dir

--- scripts/add_rg_singleton.py
+++ scripts/add_rg_singleton.py
@@ -7,6 +7,5 @@ for i in range(0, len(rg)):
     accession=rg.iloc[i,0]
     read_group=rg.iloc[i,1]
     in_file_name=str('/global/scratch/users/chandlersutherland/e12/wang_athaliana/bam_sorted_rg/' + str(accession) + '.bam')
-    out_file_name=str('/global/scratch/users/chandlersutherland/e12/wang_athaliana/mapped_reads_rg/' + str(accession) + '_rg.bam')
+    out_file_name=str('/global/scratch/users/chandlersutherland/e12/wang_athaliana/bam_sorted_rg/' + str(accession) + '_rg.bam')
     os.system("samtools addreplacerg -@ 20 -r "+read_group+" -m orphan_only -o "+out_file_name+" "+in_file_name)
-
