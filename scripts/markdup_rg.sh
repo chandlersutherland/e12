b12237535095f7d225188c8cd34c58009fa07500
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-07-20 03:53:52 -0700

    updating input, output, for neg control removing duplicate reads instead of just marking...

--- scripts/markdup_rg.sh
+++ scripts/markdup_rg.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=markdup
-#SBATCH --account=ac_kvkallow
+#SBATCH --job-name=markdup_rg
 #SBATCH --partition=savio2
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=04:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mail-user=chandlersutherland@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
@@ -14,18 +13,19 @@
 module load python
 module load samtools
 
-INPUT_DIR=/global/scratch/users/chandlersutherland/e12/wang_athaliana/bam_sorted
-OUTPUT_DIR=/global/scratch/users/chandlersutherland/e12/wang_athaliana/marked
+INPUT_DIR=/global/scratch/users/chandlersutherland/e12/wang_athaliana/bam_sorted_rg
+OUTPUT_DIR=/global/scratch/users/chandlersutherland/e12/wang_athaliana/marked_rg
 
 #with our sorted happy files, it's time to run samtools fixmate to prep and then samtools markdup to mark pcr duplicates 
- 
-for ((i = 185; i <= 248; i++))
+
+cd $INPUT_DIR 
+for f in *_rg.bam
 do 
-	echo "beginning SRR8367${i}"
+	echo "beginning ${f}"
 	#name sort then fixmate 
-	samtools sort -n -T $INPUT_DIR/temp -@ $SLURM_NTASKS $INPUT_DIR/aln_SRR8367"${i}".bam|\
+	samtools sort -n -T $INPUT_DIR/temp -@ $SLURM_NTASKS $INPUT_DIR/"${f}"|\
 	samtools fixmate -m -@ $SLURM_NTASKS - - |\
 	samtools sort -T $INPUT_DIR/temp2 -@ $SLURM_NTASKS - |\
-	samtools markdup -T $INPUT_DIR/temp3 -s -@ $SLURM_NTASKS - $OUTPUT_DIR/markdup_SRR8367"${i}".bam
-	echo "finished SRR8367${i}"
+	samtools markdup -r -T $INPUT_DIR/temp3 -s -@ $SLURM_NTASKS - $OUTPUT_DIR/"${f}"
+	echo "finished ${f}"
 done
