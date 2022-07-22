c784264fa890418351ca3f45896fe9526531f36a
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-07-22 15:42:06 -0700

    formatting gets me again

--- scripts/neg_merge.sh
+++ scripts/neg_merge.sh
@@ -4,7 +4,7 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mail-user=chandlersutherland@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
@@ -21,4 +21,4 @@ OUTPUT_DIR=/global/scratch/users/chandlersutherland/e12/wang_athaliana/neg_contr
 cd $INPUT_DIR 
 FILES=$(ls)
 
-samtools merge -b $FILES -@ $SLURM_NTASKS -o $OUTPUT_DIR/merged_marked_rg.bam 
+samtools merge -@ $SLURM_NTASKS $OUTPUT_DIR/merged_marked_rg.bam $FILES
