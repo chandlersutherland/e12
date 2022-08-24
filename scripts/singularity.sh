a03125f01b5c1036dc5041abaf852f0deaef9b17
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-08-24 09:54:51 -0700

    times up

--- scripts/singularity.sh
+++ scripts/singularity.sh
@@ -5,7 +5,6 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
-#SBATCH --time=00:30:00
 #SBATCH --mail-user=chandlersutherland@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
