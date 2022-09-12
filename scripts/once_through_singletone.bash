ae17b5421d4c87eb01b2a8f73fe342d3249b0d4b
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-09-12 12:04:19 -0700

    alingment intermediate

--- scripts/once_through_singletone.bash
+++ scripts/once_through_singletone.bash
@@ -30,8 +30,9 @@ RG=FCHC2TWBBXX_L1_wHAXPI032572-47
 accession=SRR8367191
 
 #map with bwa mem. I am not going to save intermediate files, so pipe through to sorted and indexed bam 
-bwa mem -t 12 $REF_GENOME $INPUT_DIR/"${accession}"_1.fastq $INPUT_DIR/"${accession}"_2.fastq |
-samtools view -@ $SLURM_NTASKS -b - |\
+bwa mem -t 12 $REF_GENOME $INPUT_DIR/"${accession}"_1.fastq $INPUT_DIR/"${accession}"_2.fastq > $OUTPUT_DIR/aln_"${accession}".sam
+
+samtools view -@ $SLURM_NTASKS -b aln_"${accession}".sam |\
 samtools sort -@ $SLURM_NTASKS -T $OUTPUT_DIR/temp > $OUTPUT_DIR/"${accession}".bam
 samtools index -@ $SLURM_NTASKS $OUTPUT_DIR/"${accession}".bam
 
